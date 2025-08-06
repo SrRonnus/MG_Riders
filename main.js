a32a9f08970cb4070f1cad2a3606d4a4de3906f4
Ronny Cevallos <ronnycevallos873@gmail.com>
2025-08-06 02:11:27 -0500

    agregamos el menu desplegable

---
+++ main.js
@@ -0,0 +1,15 @@
+function toggleDropdown() {
+      document.getElementById('menuDropdown').classList.toggle('show');
+    }
+    // Cierra el menú si se hace clic fuera
+    window.onclick = function(event) {
+      if (!event.target.matches('.dropdown-btn')) {
+        var dropdowns = document.getElementsByClassName("dropdown");
+        for (var i = 0; i < dropdowns.length; i++) {
+          var openDropdown = dropdowns[i];
+          if (openDropdown.classList.contains('show')) {
+            openDropdown.classList.remove('show');
+          }
+        }
+      }
+    }
